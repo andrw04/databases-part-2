d935252af31ab8a6003cf44066456c654b03ec97
Andrey Sivy <sivyandrey2021@gmail.com>
2024-02-20 01:17:10 +0300

    Added check student id trigger

--- Lab2/lab2.sql
+++ Lab2/lab2.sql
@@ -1,41 +1,34 @@
-DROP TABLE students;
-DROP TABLE groups;
+SET SERVEROUTPUT ON;
+
+--DROP TABLE students;
+--DROP TABLE groups;
 
 -- task 1
 CREATE TABLE students (
     id NUMBER PRIMARY KEY,
-    name VARCHAR2 NOT NULL,
-    group_id NUMBER FOREIGN KEY
+    name VARCHAR2(100) NOT NULL,
+    group_id NUMBER
 );
 
 CREATE TABLE groups (
     id NUMBER PRIMARY KEY,
-    name VARCHAR2 NOT NULL,
-    c_val NUMBER DEFAULT
-)
+    name VARCHAR2(100) NOT NULL,
+    c_val NUMBER DEFAULT 0
+);
 
 -- task 2
 CREATE OR REPLACE TRIGGER check_student_id
-BEFORE UPDATE OR INSERT
-
+BEFORE UPDATE OR INSERT ON students
+FOR EACH ROW
+DECLARE
+    students_count NUMBER;
+BEGIN
+    SELECT COUNT(*) INTO students_count FROM students WHERE students.id=:NEW.id;
+    
+    IF students_count != 0 THEN
+        RAISE_APPLICATION_ERROR(-20001, 'This id is already exits.');
+    END IF;
 END;
+/
 
-CREATE OR REPLACE TRIGGER increment_student_id
-BEFORE UPDATE OR INSERT
-END;
-
-CREATE OR REPLACE TRIGGER check_student_name
-BEFORE UPDATE OR INSERT
-END;
-
-CREATE OR REPLACE TRIGGER check_group_id
-BEFORE UPDATE OR INSERT
-END;
-
-CREATE OR REPLACE TRIGGER increment_group_id
-BEFORE UPDATE OR INSERT
-END;
-
-CREATE OR REPLACE TRIGGER check_group_name
-BEFORE UPDATE OR INSERT
-END;
+INSERT INTO students (id, name, group_id) VALUES (1, 'Andrei', 4);
