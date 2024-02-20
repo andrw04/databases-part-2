596a3754bb2b061db4e1cc828f541323b9ff6d6f
Andrey Sivy <sivyandrey2021@gmail.com>
2024-02-20 23:03:00 +0300

    Completed task 6

--- Lab2/lab2.sql
+++ Lab2/lab2.sql
@@ -50,8 +50,26 @@ END;
 /
 
 
+CREATE OR REPLACE TRIGGER check_group_name
+BEFORE INSERT ON groups
+FOR EACH ROW
+DECLARE
+    groups_count NUMBER;
+    exists_name EXCEPTION;
+BEGIN
+    SELECT COUNT(*) INTO groups_count FROM groups WHERE name = :NEW.name;
+    
+    IF groups_count > 0 THEN
+        DBMS_OUTPUT.PUT_LINE('Group name: ' || :NEW.name || ' already exists.');
+        RAISE exists_name;
+    END IF;
+END;
+/
+
+
+
 CREATE OR REPLACE TRIGGER check_group_id
-BEFORE UPDATE OR INSERT ON groups
+BEFORE INSERT ON groups
 FOR EACH ROW
 DECLARE
     groups_count NUMBER;
@@ -85,21 +103,6 @@ END;
 /
 
 
-CREATE OR REPLACE TRIGGER check_group_name
-BEFORE INSERT OR UPDATE ON groups
-FOR EACH ROW
-DECLARE
-    groups_count NUMBER;
-    exists_name EXCEPTION;
-BEGIN
-    SELECT COUNT(*) INTO groups_count FROM groups WHERE groups.name=:NEW.name;
-    IF groups_count > 0 THEN
-        DBMS_OUTPUT.PUT_LINE('Group name: ' || :NEW.name || ' already exists.');
-        RAISE exists_name;
-    END IF;
-END;
-/
-
 -- task 3
 CREATE OR REPLACE TRIGGER students_cascade_delete
 AFTER DELETE ON groups
@@ -109,9 +112,42 @@ BEGIN
 END;
 /
 
+
+-- task 6
+CREATE OR REPLACE TRIGGER update_c_val
+AFTER INSERT OR UPDATE OR DELETE ON students
+FOR EACH ROW
+DECLARE
+    students_in_group NUMBER;
+BEGIN
+    IF INSERTING THEN
+        UPDATE groups
+        SET c_val = c_val + 1
+        WHERE id = :NEW.group_id;
+    ELSIF DELETING THEN
+        UPDATE groups
+        SET c_val = c_val - 1
+        WHERE id = :OLD.group_id;
+    ELSIF UPDATING THEN
+        UPDATE groups
+        SET c_val = c_val - 1
+        WHERE id = :OLD.group_id;
+        
+        UPDATE groups
+        SET c_val = c_val + 1
+        WHERE id = :NEW.group_id;
+    END IF;
+EXCEPTION 
+    WHEN NO_DATA_FOUND THEN
+        DBMS_OUTPUT.PUT_LINE('The group was deleted');
+END;
+/
+
 INSERT INTO groups (id, name, c_val) VALUES (1, '153504', 0);
 INSERT INTO students (id, name, group_id) VALUES (1, 'Andrei', 1);
 INSERT INTO students (id, name, group_id) VALUES (2, 'Anton', 1);
 INSERT INTO students (id, name, group_id) VALUES (3, 'Vadim', 1);
+
+
 
 --DELETE FROM groups WHERE groups.id=1;
